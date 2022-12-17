5c4b66d137136e2c607a324908bdf55e116abbe6
Yaroslav Ageenko <54582556+TokMatok@users.noreply.github.com>
2022-12-17 09:19:35 +0300

    add new screen

--- FootballBottles/Controller/ReviewsVC.swift
+++ FootballBottles/Controller/ReviewsVC.swift
@@ -9,6 +9,70 @@ import UIKit
 
 class ReviewsVC: UIViewController {
 
+    
+    //labels
+    @IBOutlet weak var footbalLabel : UILabel!
+    @IBOutlet weak var TenisLabel : UILabel!
+    @IBOutlet weak var HockeyLabel : UILabel!
+    @IBOutlet weak var BasketballLabel : UILabel!
+    @IBOutlet weak var VolleyballLabel : UILabel!
+    @IBOutlet weak var NoviceLabel : UILabel!
+    @IBOutlet weak var SmesharikLabel : UILabel!
+    @IBOutlet weak var YesLabel : UILabel!
+    @IBOutlet weak var NoLabel : UILabel!
+    @IBOutlet weak var MaybeLabel : UILabel!
+    @IBOutlet weak var questionOneLabel : UILabel!
+    @IBOutlet weak var questionTwoLabel : UILabel!
+    @IBOutlet weak var questionThreeLabel : UILabel!
+    
+    
+    //buttonsOutlets
+    @IBOutlet weak var footballButton : UIButton!
+    @IBOutlet weak var tenisButton : UIButton!
+    @IBOutlet weak var hockeyButton : UIButton!
+    @IBOutlet weak var volleyballButton : UIButton!
+    @IBOutlet weak var basketballButton : UIButton!
+    @IBOutlet weak var noviceButton : UIButton!
+    @IBOutlet weak var smesharikButton : UIButton!
+    @IBOutlet weak var yesButton : UIButton!
+    @IBOutlet weak var noButton : UIButton!
+    @IBOutlet weak var maybeButton : UIButton!
+    
+    
+    
+    //actions
+    @IBAction func footballButton(_ sender: UIButton) {
+        self.footballButton.setImage(UIImage(named: "ReviewButtonActive"), for: .normal)
+    }
+    @IBAction func tenisButton(_ sender: UIButton) {
+        self.tenisButton.setImage(UIImage(named: "ReviewButtonActive"), for: .normal)
+    }
+    @IBAction func basketballButton(_ sender: UIButton) {
+        self.basketballButton.setImage(UIImage(named: "ReviewButtonActive"), for: .normal)
+    }
+    @IBAction func hockeyButton(_ sender: UIButton) {
+        self.hockeyButton.setImage(UIImage(named: "ReviewButtonActive"), for: .normal)
+    }
+    @IBAction func volleyballButton(_ sender: UIButton) {
+        self.volleyballButton.setImage(UIImage(named: "ReviewButtonActive"), for: .normal)
+    }
+    @IBAction func noviceButton(_ sender: UIButton) {
+        self.noviceButton.setImage(UIImage(named: "ReviewButtonActive"), for: .normal)
+    }
+    @IBAction func smesharikButton(_ sender: UIButton) {
+        self.smesharikButton.setImage(UIImage(named: "ReviewButtonActive"), for: .normal)
+    }
+    @IBAction func yesButton(_ sender: UIButton) {
+        self.yesButton.setImage(UIImage(named: "ReviewButtonActive"), for: .normal)
+    }
+    @IBAction func noButton(_ sender: UIButton) {
+        self.noButton.setImage(UIImage(named: "ReviewButtonActive"), for: .normal)
+    }
+    @IBAction func maybeButton(_ sender: UIButton) {
+        self.maybeButton.setImage(UIImage(named: "ReviewButtonActive"), for: .normal)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -34,3 +98,6 @@ class ReviewsVC: UIViewController {
     */
 
 }
+
+
+
---
+++ Pods/Target Support Files/Pods-FootballBottles/Pods-FootballBottles-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FootballBottles : NSObject
+@end
+@implementation PodsDummy_Pods_FootballBottles
+@end
